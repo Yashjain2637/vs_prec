a527f235a11b5c6491eec2ba2acc5df7cf8e8ea7
Yash Jain <yashjain2637@gmail.com>
2026-02-13 13:07:33 +0530

    sklearn-added

--- main.py
+++ main.py
@@ -2,4 +2,22 @@ import numpy as np
 import pandas as pd
 
 df=pd.read_csv('C:\\Users\\Lenovo\\Desktop\\vs__prec\\covid_toy - covid_toy.csv')
+# print(df.head(2))
+
+df=df.dropna()
 print(df.head(2))
+
+x=df.drop(columns=['has_covid'])
+y=df['has_covid']
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
+
+from sklearn.preprocessing import LabelEncoder
+
+lb=LabelEncoder()
+x_train['gender']=lb.fit_transform(x_train['gender'])
+x_train['cough']=lb.fit_transform(x_train['cough'])
+x_train['city']=lb.fit_transform(x_train['city'])
+
+print(x_train.head(3))
